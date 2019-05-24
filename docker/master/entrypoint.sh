b0fe4b7394a37eb7f56929d44c3a0d8870a27769
Diego Ferigo <diego.ferigo@iit.it>
2019-05-24 07:34:04 +0200

    Added master image

---
+++ docker/master/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+set -u
+
+echo "==> Executing master image entrypoint ..."
+
+echo "-> Setting up ROS"
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+
+echo "==> Container ready"
+exec "$@"
